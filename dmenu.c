ca3c415b41c1e6b006a83faba8101abbe3cb3124
bakkeby <bakkeby@gmail.com>
2022-03-27 22:40:28 +0200

    highpriority: adding check for malloc failure + cleanup
    ref. https://git.suckless.org/sites/commit/9bbc02d13a89e427a046817771c6f8fd332d0167.html

--- dmenu.c
+++ dmenu.c
@@ -151,6 +151,9 @@ cleanup(void)
 	XUngrabKey(dpy, AnyKey, AnyModifier, root);
 	for (i = 0; i < SchemeLast; i++)
 		free(scheme[i]);
+	for (i = 0; i < hplength; ++i)
+		free(hpitems[i]);
+	free(hpitems);
 	drw_free(drw);
 	XSync(dpy, False);
 	XCloseDisplay(dpy);
