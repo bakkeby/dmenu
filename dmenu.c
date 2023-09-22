7534a2d5ba0909932b8a14fa3f747fdf30fb168c
bakkeby <bakkeby@gmail.com>
2023-09-22 18:29:18 +0200

    drw: minor improvement to the nomatches cache
    1. use `unsigned int` to store the codepoints, this avoids waste on
       common case where `long` is 64bits. and POSIX guarantees `int` to be
       at least 32bits so there's no risk of truncation.
    2. since switching to `unsigned int` cuts down the memory requirement by
       half, double the cache size from 64 to 128.
    3. instead of a linear search, use a simple hash-table for O(1) lookups.
    
    ref.
    https://git.suckless.org/dmenu/commit/7ab0cb5ef0e19352fc5d64ae0d57a5cf4540acbf.html

--- dmenu.c
+++ dmenu.c
@@ -22,7 +22,6 @@
 /* macros */
 #define INTERSECT(x,y,w,h,r)  (MAX(0, MIN((x)+(w),(r).x_org+(r).width)  - MAX((x),(r).x_org)) \
                              * MAX(0, MIN((y)+(h),(r).y_org+(r).height) - MAX((y),(r).y_org)))
-#define LENGTH(X)             (sizeof X / sizeof X[0])
 #define TEXTW(X)              (drw_fontset_getwidth(drw, (X)) + lrpad)
 #define OPAQUE 0xffU
 #define CLEANMASK(mask)       (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
